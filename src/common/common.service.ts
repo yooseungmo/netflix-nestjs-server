c33e85f69a365269d7b15b11cf9b77571e7988f8
Blanc <topblade6@naver.com>
2025-02-09 11:59:53 +0900

    feature: migrate S3 operations to AWS SDK v3
    - Replace AWS SDK v2 with v3 for S3 operations
    - Update S3 initialization to use AWS SDK v3 client

--- src/common/common.service.ts
+++ src/common/common.service.ts
@@ -1,6 +1,7 @@
+import { ObjectCannedACL, PutObjectCommand, S3 } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import * as AWS from 'aws-sdk';
 import { SelectQueryBuilder } from 'typeorm';
 import { v4 as Uuid } from 'uuid';
 import { envVariableKeys } from './const/env.const';
@@ -9,38 +10,33 @@ import { PagePaginationDto } from './dto/page-pagination.dto';
 
 @Injectable()
 export class CommonService {
-  private s3: AWS.S3;
+  private s3: S3;
 
   constructor(private readonly configService: ConfigService) {
-    AWS.config.update({
+    this.s3 = new S3({
       credentials: {
         accessKeyId: configService.get<string>(envVariableKeys.awsAccessKeyId),
         secretAccessKey: configService.get<string>(envVariableKeys.awsSecretAccessKey),
       },
+
       region: configService.get<string>(envVariableKeys.awsRegion),
     });
-
-    this.s3 = new AWS.S3();
   }
 
   async saveMovieToPermanentStorage(fileName: string) {
     try {
       const bucketName = this.configService.get<string>(envVariableKeys.bucketName);
-      await this.s3
-        .copyObject({
-          Bucket: bucketName,
-          CopySource: `${bucketName}/public/temp/${fileName}`,
-          Key: `public/movie/${fileName}`,
-          ACL: 'public-read',
-        })
-        .promise();
+      await this.s3.copyObject({
+        Bucket: bucketName,
+        CopySource: `${bucketName}/public/temp/${fileName}`,
+        Key: `public/movie/${fileName}`,
+        ACL: 'public-read',
+      });
 
-      await this.s3
-        .deleteObject({
-          Bucket: bucketName,
-          Key: `public/temp/${fileName}`,
-        })
-        .promise();
+      await this.s3.deleteObject({
+        Bucket: bucketName,
+        Key: `public/temp/${fileName}`,
+      });
     } catch (e) {
       console.log(e);
       throw new InternalServerErrorException('S3 에러!!!');
@@ -51,12 +47,13 @@ export class CommonService {
     const params = {
       Bucket: this.configService.get<string>(envVariableKeys.bucketName),
       Key: `public/temp/${Uuid()}.mp4`,
-      Expires: expiresIn,
-      ACL: 'public-read',
+      ACL: ObjectCannedACL.public_read,
     };
 
     try {
-      const url = await this.s3.getSignedUrlPromise('putObject', params);
+      const url = await getSignedUrl(this.s3, new PutObjectCommand(params), {
+        expiresIn,
+      });
 
       return url;
     } catch (e) {
